52c8eb4a2a5e6e2ed99856e27770cdc80f7e694d
bendjelloul noureddine <bendjelloulnoureddine@gmail.com>
2024-12-24 13:00:41 +0100

    Use CBV in the Jobs views

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import JobListView
+from .views import (
+    JobListView,
+    JobDetailView,
+    JobCreateView
+)
 
 urlpatterns = [
-    path("", JobListView.as_view(), name="job-list")
+    path("", JobListView.as_view(), name="job-list"),
+    path("create/", JobCreateView.as_view(), name="job-create"),
+    path("detail/<pk>", JobDetailView.as_view(), name="job-detail"),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import  (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Job
 
 
@@ -8,3 +15,19 @@ class JobListView(ListView):
     template_name = "jobs/job-list.html"
     context_object_name = "jobs"
     queryset = Job.objects.all()
+
+
+class JobDetailView(DetailView):
+    model = Job
+    template_name = "jobs/job-detail.html"
+    
+
+class JobCreateView(CreateView):
+    model = Job
+    template_name = "jobs/job-create.html"
+    fields = ["title", "recruter", "salary", "location", "category" ,"job_types"]
+    success_url = reverse_lazy('job-list')
+
+
+    def form_valid(self, form):
+        return super().form_valid(form)
